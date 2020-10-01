aff0fe43070c582553f0c6bc40a59ee9ae41dd62
Myyk Seok <myyk.seok@gmail.com>
2020-10-01 16:13:50 +0800

    update about

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-    * Start Bootstrap - Agency v6.0.2 (https://startbootstrap.com/template-overviews/agency)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+    * Start Bootstrap - DadfinderBiz v6.0.2 (https://startbootstrap.com/template-overviews/agency)
+    * Copyright 2013-2020 Myyk Seok
+    * Licensed under MIT (https://github.com/StartBootstrap/dadfinder-biz/blob/master/LICENSE)
     */
     (function ($) {
     "use strict"; // Start of use strict
