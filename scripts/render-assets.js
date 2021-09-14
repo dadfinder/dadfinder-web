feda9a7ca2069f21acb95be0166a85df29192ec2
Myyk Seok <myyk.seok@gmail.com>
2021-09-14 06:29:56 +0000

    fix the way CNAME is deleted by scripts and expose another gitpod port

--- scripts/render-assets.js
+++ scripts/render-assets.js
@@ -8,4 +8,5 @@ module.exports = function renderAssets() {
     const destPath = path.resolve(path.dirname(__filename), '../docs/.');
 
     sh.cp('-R', sourcePath, destPath)
+    sh.cp('-R', path.resolve(path.dirname(__filename), '../src/CNAME'), destPath)
 };
